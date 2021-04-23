59be9c55010612748e30c0d8495a84fe72b150b5
Rishabh <rishabhprakash01@gmail.com>
2021-04-24 02:17:23 +0530

    Deleting the third line

--- src/MVP.java
+++ src/MVP.java
@@ -4,6 +4,6 @@ public class MVP
     {
         System.out.println("Hello World");
         System.out.println("This is the second line of code");
-        System.out.println("This is the third line");
+        //System.out.println("This is the third line");
     }
 }
